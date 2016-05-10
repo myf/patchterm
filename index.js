fee89dcdb2739803637cb78a9a5d34d27191bfb6
myf <myf@worldtalks.org>
2016-05-10 17:57:04 -0400

    pull as you go

--- index.js
+++ index.js
@@ -1,36 +1,41 @@
 var pull = require('pull-stream');
+var paramap = require('pull-paramap');
 var ssbclient = require('ssb-client');
 var chalk = require('chalk');
-var boxen = require('boxen');
 
 var sbot_cont = function(cb) {
-    ssbclient(function(err, sbot) {
-        if (err) throw err;
-        cb(sbot);
-    });
+  ssbclient(function(err, sbot) {
+    if (err) throw err;
+      cb(sbot);
+  });
 };
 
 var list_messages = function() {
-    sbot_cont(function(sbot) {
-        pull(
-                sbot.createLogStream(),
-                pull.collect(function(err, msgs) {
-                    msgs.map(function(item) {
-                        var cont = item.value.content;
-                        if (cont.type === 'post') {
-                            console.log(
-                                    chalk.bold.blue(item.value.author + ':')
-                            );
-                            try{
-                                console.log(boxen(cont.text, {padding: 1}));
-                            } catch(e) {
-                                console.log(e);
-                            }
-                        }
-                    });
-                })
+  sbot_cont(function(sbot) {
+    pull(
+      sbot.createLogStream(),
+      //////
+      paramap(function(item, cb) {
+        var cont = item.value.content;
+        if (cont.type === 'post') {
+            console.log(
+              chalk.bold.blue(item.value.author + ':')
             );
-        });
+            console.log(cont.text);
+            console.log();
+            cb(null, 'a');
+        }
+        /*
+            var box = boxen(cont.text, {padding: 1});
+         */
+      }, 1e13),
+      /////
+      pull.collect(function(err, msgs) {
+        if (err) throw err;
+        console.log("total: %s messages", msgs.length);
+      })
+    );
+  });
 };
 
 
